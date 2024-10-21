0cf7c45438d9a2fb9c03dd54353e5a83b144a6a9
SamVargas <samuel.vargas02@uptc.edu.co>
2024-10-21 10:15:46 -0500

    Update @

--- demo/src/main/java/com/facturacion/demo/model/Producto.java
+++ demo/src/main/java/com/facturacion/demo/model/Producto.java
@@ -3,14 +3,18 @@ package com.facturacion.demo.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Producto {
 
     @Id
